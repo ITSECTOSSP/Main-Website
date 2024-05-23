8564f97b8f95c9ee83993b91570cf1cf77aea4c4
ITSECTOSSP <itsectossp@gmail.com>
2024-05-24 04:02:29 +0800

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,11 +24,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 paginatedItems.forEach((item, index) => {
                     const row = document.createElement('tr');
                     row.innerHTML = `
-                        <th scope="row">${start + index + 1}</th>
+                        <th style="display:none;" scope="row">${start + index + 1}</th>
                         <td>${item.Sp}</td>
                         <td>${item["PO No"]}</td>
                         <td>${item.Title}</td>
                         <td>
+
+                        
                             <button class="btn btn-success" style="margin-left: 5px;" type="button" data-link="${item.Iframe}/edit">
                                 <i class="glyphicon glyphicon-link" style="font-size: 30px;"></i>
                             </button>
